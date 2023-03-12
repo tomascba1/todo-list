927d3f7a85b1ae2a9d2d1df78e31618aca81366c
Tomascba <tomialonsoo.96@gmail.com>
2023-03-12 12:06:01 -0300

    custom hook added

--- src/componentes/TodoApp.jsx
+++ src/componentes/TodoApp.jsx
@@ -1,51 +1,61 @@
-import React, { useState } from 'react';
-import { Todo } from './Todo';
-import "./todo.css"
+import React, { useState } from "react";
+import { Todo } from "./Todo";
+import "./todo.css";
+import { useLocalStorage } from "../customHooks/useStorage";
 
 const TodoApp = () => {
-    const [title, setTitle] = useState('')
-    const [todo, setTodo] =useState([])
+  const [title, setTitle] = useState("");
+  const [todo, setTodo] = useLocalStorage("todoList", []);
 
-    const handleChange = (e) => {
-        const value = e.target.value
-        setTitle(value)
+  const handleChange = (e) => {
+    const value = e.target.value;
+    setTitle(value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const newTodo = {
+      id: crypto.randomUUID(),
+      title: title,
+      completed: false,
+    };
+    if (title !== "") {
+      setTodo([...todo, newTodo]);
+      setTitle("");
     }
-    const handleSubmit = (e) =>{
-        e.preventDefault()
-
-        const newTodo = {
-            id: crypto.randomUUID(),
-            title: title,
-            completed: false
-        }
-
-    setTodo([...todo, newTodo])
-    setTitle("")
+  };
+  
+  function handleUpdate(id, newValue) {
+      const temp = [...todo];
+      const item = temp.find((item) => item.id === id);
+      item.title = newValue;
+      setTodo(temp);
     }
-    function handleUpdate(id, newValue){
-        const temp = [...todo]
-        const item = temp.find(item => item.id === id)
-        item.title = newValue
-        setTodo(temp)
-    }
-    function handleDelete(id){
-        const temp = todo.filter(item => item.id !== id)
-        setTodo(temp)
+    function handleDelete(id) {
+        const temp = todo.filter((item) => item.id !== id);
+        setTodo(temp);
     }
 
-    return(
-        <div className='todoContainer border'>
-            <h1>To Do List</h1>
-            <form action="" onSubmit={handleSubmit} className="todoCreateForm">
-                <input className='todoInput' type="text" autoFocus value={title} onChange={handleChange}/>
-                <input className='buttonCreate' type="submit" onClick={handleSubmit} />
-            </form>
-            <div className='todoItem'>
-                {todo.map(item => (
-                    <Todo item={item} onUpdate={handleUpdate} onDelete={handleDelete}/>))}
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="todoContainer border">
+      <h1>To Do List</h1>
+      <form action="" onSubmit={handleSubmit} className="todoCreateForm">
+        <input
+          className="todoInput"
+          type="text"
+          autoFocus
+          value={title}
+          onChange={handleChange}
+        />
+        <input className="buttonCreate" type="submit" onClick={handleSubmit} />
+      </form>
+      <div className="todoItem">
+        {todo.map((item) => (
+          <Todo key={item.id} item={item} onUpdate={handleUpdate} onDelete={handleDelete} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export { TodoApp }
+export { TodoApp };
